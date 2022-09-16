dce78f11307523e98bee3305a44a0d66fe9ea955
pradeep <ppadmukhi28@gmail.com>
2022-09-16 20:30:53 +0530

    db name change in main.js

--- mongo_filtering/main.js
+++ mongo_filtering/main.js
@@ -42,8 +42,8 @@ const findCount = async (insertDb) => {
 const main = async () => {
   const dbInstance = await connectDB();
   const insertDb = dbInstance.db("insertdb").collection("collection");
-  const findDb = dbInstance.db("msgdb").collection("msgcoll");
-  // inserttoDb(findDb, userdata);
+  const findDb = dbInstance.db("vivasmpp_20220523").collection("msgcoll");
+  inserttoDb(findDb, userdata);
   let bigarr = [];
   let stream = findDb.find().stream();
   stream
@@ -53,21 +53,21 @@ const main = async () => {
       data.msg = encrypedData.encryptData(data.msg);
       bigarr.push(data);
       if (bigarr.length == 20000) {
-        inserttoDb(insertDb, bigarr);
+        // inserttoDb(insertDb, bigarr);
         findCount(insertDb);
         bigarr = [];
       }
     })
     .on("end", () => {
       if (bigarr.length) {
-        inserttoDb(insertDb, bigarr);
+        // inserttoDb(insertDb, bigarr);
         findCount(insertDb);
         console.log("-----end of process-----");
       }
     });
 };
 
-//  main();
+ main();
 
 
 // let start = performance.now()
